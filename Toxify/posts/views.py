a44eeb38a5e1785022b655ee59554598725fe993
fwss09 <andreipopivnich@gmail.com>
2026-02-21 21:55:51 +0200

    added main app and pages

--- Toxify/posts/views.py
+++ Toxify/posts/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, CreateView, UpdateView, ListView
 
 # Create your views here.
+class IndexView(TemplateView):
+    template_name = 'posts/index.html'
