996d0a0d6d3b81436c145db501bb9dd260f8ed6f
smotr <smorov.yaroslav2504@gmail.com>
2026-02-25 17:49:30 +0200

    settings

--- Toxify/profiles/urls.py
+++ Toxify/profiles/urls.py
@@ -1,9 +1,12 @@
+from django.contrib.auth.views import LogoutView, LoginView
 from django.urls import path
 from . import views
 
 urlpatterns = [
     # Реєстрація
     path("register/", views.RegisterView.as_view(), name="register"),
+    path('login/', LoginView.as_view(template_name='profiles/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 
     # Профіль
     path("profile/edit/", views.ProfileEditView.as_view(), name="profile_edit"),
