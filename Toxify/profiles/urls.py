f9ecb2261ee650a7035934f4f713fe7f0ea11a93
smotr <smorov.yaroslav2504@gmail.com>
2026-02-25 13:52:28 +0200

    added users, login, and registration

--- Toxify/profiles/urls.py
+++ Toxify/profiles/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 
-from profiles.views import MyProfileView
+from profiles.views import MyProfileView, register
 
 urlpatterns = [
     path('my_profile/', MyProfileView.as_view(), name='my_profile'),
+    path('register/', register, name='register'),
+    path('login/', LoginView.as_view(template_name='profiles/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
